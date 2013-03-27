49e8c4fd283f955780a9fe92451d7ee7cc93bfea
szheng1990 <zhengshu@mit.edu>
2013-03-27 18:13:47 -0400

    added tests for user; modified fixtures

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,17 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "a user should enter a first name"
+    user = User.new
+    assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should have a unique profile name"
+    user = User.new
+    user.profile_name = users(:jason).profile_name
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+
 end
